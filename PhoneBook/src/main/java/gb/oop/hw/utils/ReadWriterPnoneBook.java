4b78841f5d57f50e3418a9d11e9e94f0e53064bc
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-12-07 08:20:01 +0300

    continued

--- PhoneBook/src/main/java/gb/oop/hw/utils/ReadWriterPnoneBook.java
+++ PhoneBook/src/main/java/gb/oop/hw/utils/ReadWriterPnoneBook.java
@@ -2,7 +2,7 @@ package gb.oop.hw.utils;
 
 import gb.oop.hw.data.PhoneNote;
 
-public interface ReadWriterPnoneBook {
+public interface ReadWriter {
     public void writeToJson();
     public PhoneNote readFromJson();
 }
